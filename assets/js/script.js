d00dad81a2c5319e892f9cc0d7c3ef9783926a7e
Matt Linder <mjlinder218@gmail.com>
2021-04-29 11:29:47 -0700

    Now dynamically adding data area (upper right)

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,10 @@ searchButton.on("click", function(event){
   let apiCall = `http://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=ab49e3c91f890388f51d73286073c3a1`
 
   cityNumber++;
-  let listItem= $("<a>")
+
+  // Builds these:
+  // <a class="list-group-item list-group-item-action active" id="list-city1-list" data-bs-toggle="list" href="#list-city1" role="tab" aria-controls="city1">city1</a>
+  let listItem = $("<a>")
       .addClass("list-group-item list-group-item-action")
       .attr("blah", "****************************")
       .attr("id", "list-city"+cityNumber+"-list") //******
@@ -17,13 +20,23 @@ searchButton.on("click", function(event){
       .attr("href", "#list-city"+cityNumber) // ****
       .attr("role", "tab")
       .attr("aria-controls", "city"+cityNumber)  // **** - don't hard code
-      .prependTo(".list-group")
+      .prependTo(".list-group");
+  let listItemText  = $("#list-city"+cityNumber+"-list");
+  listItemText.text(searchInput);
 
-      let listItemText  = $("#list-city"+cityNumber+"-list");
-      listItemText.text(searchInput);
+  // Builds these:
+  // <div class="tab-pane fade" id="list-city2" role="tabpanel" 
+  // aria-labelledby="list-city2-list">city2...</div>
+  let tabContItem = $("<div>")
+      .addClass("tab-pane fade")
+      .attr("id", "list-city"+cityNumber)
+      .attr("role", "tabpanel")
+      .attr("aria-labelledby", "list-city"+cityNumber+"-list")
+      .prependTo(".tab-content");
+  let tabContItemText  = $("#list-city"+cityNumber);
+  tabContItemText.text(searchInput);
 
 })
 
-// <a class="list-group-item list-group-item-action active" id="list-city1-list" data-bs-toggle="list" href="#list-city1" role="tab" aria-controls="city1">city1</a>
 
 
