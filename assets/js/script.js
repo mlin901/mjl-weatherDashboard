550938684dc3daa6c0afbd080578f96cfda32641
Matt Linder <mjlinder218@gmail.com>
2021-04-30 07:06:16 -0700

    Started refactoring - separating fetch.then

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,22 +2,43 @@
 let searchButton = $(".btn");
 let cityNumber = 0;
 
+function getData(apiCall, searchInput) {
+  fetch(apiCall)
+  .then(function(response){
+    if (response.status == 200) {
+        response.json().then(function (data) {
+          console.log("*******data*******");
+          console.log(data);
+            displaySearchInfo(data, searchInput)  //  **********************
+        })
+    } else {
+        alert("No results for " + searchInput);
+    }
+  })
+  .catch(function(){
+      console.log("Bad Request")
+  })
+}
 
+// Evemt listener for Search button clicks
 searchButton.on("click", function(event){
   event.preventDefault();
-  console.log("*****");
-  console.log(event);
+  // console.log("*****");
+  // console.log(event);
   let searchInput =  $("#city-search").val();
   if (!searchInput) {
     alert("Enter a city name or choose one from the list.");
     return;
   }
-  console.log(searchInput);
+  // console.log(searchInput);
   let apiCall = `http://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=ab49e3c91f890388f51d73286073c3a1`
-
   cityNumber++;
 
-  // The following uilds these:
+  getData(apiCall, searchInput);
+})
+
+function displaySearchInfo(data, searchInput) {
+  // The following builds these:
   // <a class="list-group-item list-group-item-action active" id="list-city1-list" data-bs-toggle="list" href="#list-city1" role="tab" aria-controls="city1">city1</a>
   // See https://getbootstrap.com/docs/5.0/components/list-group/#javascript-behavior
   // And note that the active item gets an "active" class (in addition to the classes added bdlow). This Bootstrap behavior.
@@ -32,8 +53,7 @@ searchButton.on("click", function(event){
       .prependTo(".list-group");
   let listItemText  = $("#list-city"+cityNumber+"-list");
   listItemText.text(searchInput);
-
-  // The following uilds these:
+  // The following builds these:
   // <div class="tab-pane fade" id="list-city2" role="tabpanel" aria-labelledby="list-city2-list">city2...</div>
   // See https://getbootstrap.com/docs/5.0/components/list-group/#javascript-behavior
   // And note that the active item gets an "active" class (in addition to the classes added below). This is Bootstrap behavior.
@@ -48,19 +68,34 @@ searchButton.on("click", function(event){
 
   $("#list-city"+cityNumber+"-list")[0].click();
   $("#city-search").val("");
-})
+}
 
-let listClickEl = $(".list-group");
 
+
+// ******************************************************************
+// ******************************************************************
+
+
+// ******Add API search bit to the following (cf. getData and the call to that function)
+//      Then split out display method, as I did with the above
+
+let listClickEl = $(".list-group");  
+// Event listener for list item clicks
 listClickEl.on("click", function(event){
   event.preventDefault();
+  var clickedItem = event.target;
+  // console.log("##############");
+  // console.log(event);
+  // console.log(clickedItem);
 
   // Remove cards if there are any
   if ($(".cardCol")){
     $( "#cards" ).empty();
   }
+  console.log("blah");
+  
   // Create cards for the next 5 days
-  for (var i = 0; i < 5; i++) {
+  for (var i = 0; i < 5; i++) {  // *******5 shouldn't be hc
     let cardDiv1 = $("<div>")
         .addClass("col-sm-2 cardCol")
         .appendTo("#cards");
@@ -75,13 +110,8 @@ listClickEl.on("click", function(event){
         .appendTo(cardDiv3);
     cardHead.text("Tomorrow"); // ***not hc
     let cardP = $("<p>")
-        .addClass("ard-text")
+        .addClass("card-text")
         .appendTo(cardDiv3);
     cardP.text("Stuff for Tomorrow"); // ***not hc
-
-      
   }
-  
-
-
 })
